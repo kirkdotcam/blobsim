75bcb8ed662d96e320d9f92c526c47c1ffd9e470
camkirk <camden.kirkland@gmail.com>
2019-12-19 19:10:41 -0600

    one dead herbivore

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import random
 
 food = 100
+population = []
+
+for x in range(5):
+    population.append({
+        "hungry":True,
+        "type":"herbivore",
+        "alive":True
+    })
+
+def eat(blob):
+    global food
+
+    if blob["hungry"]:
+        if blob["alive"] and food > 1:
+            food -= 2
+            print(food)
+        else:
+            blob["alive"] = False
+            print(population)
+            exit()
+
+    if blob["alive"] and not blob["hungry"]:
+        blob["hungry"] = True
 
 while True:
-    
-    if food < 1:
-        print("blob dies")
-        exit()
-    else:
-        food-=1
-        print(food)
+    for blob in population:
+        eat(blob)
+
