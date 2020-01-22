137d5e3fc0eff9e1c6f55772d8a36fc7b2874b32
CamKirk <camden.kirkland@gmail.com>
2020-01-22 13:01:52 -0600

    fix deepcopy issue

--- main.py
+++ main.py
@@ -5,4 +5,15 @@ sim = Sim.Simulation()
 
 print(sim.population[0].__dict__)
 # print(sim.population[0])
+
 data = sim.run(days=10)
+
+
+# data = []
+# for day in range(10):
+#     daydata = sim.run_day()
+#     data.append(daydata)
+
+print("run")
+for run in data:
+    print(run[0])
--- src/Simulation.py
+++ src/Simulation.py
@@ -1,35 +1,33 @@
 from .Blob import Blob
+from copy import deepcopy
 
 
 class Simulation():
 
     def __init__(self, members=200, food=20000):
-        self.population = Blob.gen_population(self,members=members, food=food)
+        self.population = Blob.gen_population(self, members=members, food=food)
         self.current_day = 0
 
     def current_config(self):
         print(self.__dict__)
 
-    def irun(self, days=10):
-        yield self.pop_survival(days)
+    def run_day(self):
+
+        self.current_day += 1
+
+        for blob in self.population:
+            blob.survive(self.population)
+
+        return [blob.report() for blob in self.population]
 
     def run(self, **kwargs):
         results = []
+        days = kwargs.get("days", 30)
+
         for x in range(days):
-            results.append(next(self.irun(days=days)))
-            
+            results.append(deepcopy(self.run_day()))
+
         return results
-
-    def pop_survival(self, days):
-        print("begin run")
-
-        for day in range(days):
-            self.current_day += 1
-
-            for blob in self.population:
-                blob.survive(self.population)
-
-        return [blob.report() for blob in self.population]
 
     def report(self):
         for blob in self.population:
